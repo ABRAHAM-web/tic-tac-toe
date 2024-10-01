ae329cab3d4a4e80bf396c9e1398c52cd4bbccdb
Abraham <localaxe@gmail.com>
2024-10-01 14:27:50 +0200

    Updated from ubuntu. Mont crashed

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,16 +1,31 @@
+import { useState } from "react";
+
 const initialGameBoard = [
         [null,null,null],
-        [null,null,null],
-        [null,null,null],
+        [null,"o",null],
+        [null,null,"X"],
     ];
 
 export default function GameBoard () {
+    let [gameBoard,setGameBoard] = useState(initialGameBoard);
+
+    function handleSelectSquare (rowIndex,colIndex) {
+        setGameBoard((previousGameBoard) => {
+
+            const updatedBoard = [...previousGameBoard.map(innerArray => [...innerArray])];
+            //const updatedBoard = gameBoard;
+            updatedBoard[rowIndex][colIndex] = "X";
+        })
+        return 
+            updatedBoard;
+            
+    }
 
     return <ol id = "game-board">
-            {initialGameBoard.map((row,rowIndex)=>(<li key={rowIndex}>
+            {gameBoard.map((row,rowIndex)=>(<li key={rowIndex}>
                 <ol>
                     {row.map((playerSymbol,colIndex)=>(<li key={colIndex}> 
-                         <button>{playerSymbol}</button>       
+                         <button >{playerSymbol}</button>       
                     </li>))}
                 </ol>
                  
